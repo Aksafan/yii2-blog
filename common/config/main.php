0356a5014a3d92cae9cfe46fa23714e92a4c4c1d
Aksafan <subzero_ck@mail.ru>
2016-01-08 19:24:02 +0200

    Added and configured AdminLTE panel

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,11 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => false,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
