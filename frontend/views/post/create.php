6540ec93af0e6fe242320b2c707082512e8daa82
Aksafan <subzero_ck@mail.ru>
2016-01-10 17:58:52 +0200

    Fixed post creating)

--- frontend/views/post/create.php
+++ frontend/views/post/create.php
@@ -5,6 +5,7 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Post */
+/* @var $modelCategory array */
 
 $this->title = Yii::t('app', 'Create Post');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Posts'), 'url' => ['index']];
@@ -16,6 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= $this->render('_form', [
         'model' => $model,
+        'modelCategory' => $modelCategory
     ]) ?>
 
 </div>
